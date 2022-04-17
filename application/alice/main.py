c53457da1c7c6f97bbfdac84708b86f55565b13c
Leonardo Benitez <lsbenitezpereira@gmail.com>
2022-04-17 18:32:53 -0300

    testing webservers with docker-compose

--- application/alice/main.py
+++ application/alice/main.py
@@ -4,8 +4,8 @@ import uvicorn
 app = fastapi.FastAPI()
 
 @app.get("/")
-async def root() -> None:
-    return 'hello'
+async def root():
+    return fastapi.responses.PlainTextResponse('hello')
     
 if __name__ == "__main__":
     uvicorn.run('main:app', host="0.0.0.0", port=80, reload=True, debug=True)
--- application/bob/main.py
+++ application/bob/main.py
@@ -5,9 +5,9 @@ import requests
 app = fastapi.FastAPI()
 
 @app.get("/")
-async def root() -> None:
+async def root():
     r = requests.get('http://alice/')
-    return r.content + 'world'
+    return fastapi.responses.PlainTextResponse(r.content.decode('utf-8') + ' world')
     
 if __name__ == "__main__":
     uvicorn.run('main:app', host="0.0.0.0", port=80, reload=True, debug=True)
