90305351ff97c9d273b77843b1c678934c59c159
Maximilian Zinke <maximilian.zinke@skaylink.com>
2022-05-10 10:40:23 +0200

    feat: pro script for building and pushing
    CI would be more better, but is okay for now ;)

--- apps/build_and_push.sh
+++ apps/build_and_push.sh
@@ -1,20 +1,22 @@
+#!/bin/bash
+set -e
+
 REGISTRY=skaylinkgmbh
-IMAGE_NAME=ix-multicloud-demo-petra
+
+function push_image () {
+  cd ${FOLDER}
+  docker build -t ${IMAGE_NAME} .
+  docker tag ${IMAGE_NAME} ${REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
+  docker push ${REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
+  cd ..
+}
+
+IMAGE_NAME="ix-multicloud-demo-petra"
 IMAGE_VERSION=latest
 FOLDER=petra
-cd ${FOLDER}
-docker build -t ${IMAGE_NAME} .
-docker tag ${IMAGE_NAME} ${REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
-docker push ${REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
-cd ..
+push_image
 
-
-REGISTRY=skaylinkgmbh
-IMAGE_NAME=ix-multicloud-demo-klaus
+IMAGE_NAME="ix-multicloud-demo-klaus"
 IMAGE_VERSION=latest
 FOLDER=klaus
-cd ${FOLDER}
-docker build -t ${IMAGE_NAME} .
-docker tag ${IMAGE_NAME} ${REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
-docker push ${REGISTRY}/${IMAGE_NAME}:${IMAGE_VERSION}
-cd ..
+push_image
