f43c3b352153bc7dd08106d1cca1e4a8fa7678ee
Maximilian Zinke <maximilian.zinke@skaylink.com>
2022-06-09 13:53:08 +0200

    fix: cluster definition (only enable aks)

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -6,27 +6,27 @@ locals {
 # Of cause it's not really efficient to create all three clusters at once,
 # for our test case we anyway will not keep the clusters for that long.
 
-module "aws_eks" {
-  source = "./cluster/aws"
+# module "aws_eks" {
+#   source = "./cluster/aws"
 
-  cluster_name    = locals.cluster_name
-  cluster_version = locals.cluster_version
-}
+#   cluster_name    = local.cluster_name
+#   cluster_version = local.cluster_version
+# }
 
 module "azure_aks" {
   source = "./cluster/azure"
 
-  cluster_name    = locals.cluster_name
-  cluster_version = locals.cluster_version
+  cluster_name    = local.cluster_name
+  cluster_version = local.cluster_version
 
   azure_region = var.azure_region
 }
 
-module "gcp_gke" {
-  source = "./cluster/gcp"
+# module "gcp_gke" {
+#   source = "./cluster/gcp"
 
-  cluster_name    = locals.cluster_name
-  cluster_version = locals.cluster_version
+#   cluster_name    = local.cluster_name
+#   cluster_version = local.cluster_version
 
-  gcp_region = var.gcp_region
-}
+#   gcp_region = var.gcp_region
+# }
